5962e055dcc68db56325d46745fe4fb6a33c7abd
ArnabSaga <arnabdey15091@gmail.com>
2026-03-21 18:55:10 +0600

    feat: initial commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
+import { Button } from '@/components/ui/button';
+
 export default function Home() {
   return (
     <div>
       <h2>Bye Bye World</h2>
+      <Button>Click Me</Button>
    </div>
   );
 }
